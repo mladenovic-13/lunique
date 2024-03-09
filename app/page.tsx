300616f978f4b624ce226b2af69e0f40a6cea20a
Nikola Mladenović <nikolanik999@gmail.com>
2024-03-09 19:59:47 +0100

    fix: theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Header from "@/components/Header"
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
+    <div className="flex-1 w-full flex flex-col items-center">
+      <div className="animate-in flex-1 flex items-center justify-center flex-col opacity-0 max-w-4xl px-3">
         <Header />
       </div>
     </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import logoImg from "@/public/lunique-logo-light.png"
 export default function Header() {
   return (
     <div className="flex flex-col items-center">
-      <div className="p-10">
+      <div className="p-5">
         <Image src={logoImg} alt="Lunique Logo" width={300} height={150} />
       </div>
       <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
